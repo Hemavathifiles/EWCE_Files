55ebe8c20fa5a8a708d161b33a4c212451e32f77
hemav <hemav@LAPTOP-BH7SCHNS>
2025-04-07 20:31:59 +0530

    third commit

---
+++ ewce_codes/src/ewce_codes/DemoOnClass.java
@@ -0,0 +1,26 @@
+package ewce_codes;
+
+
+ class Student
+{
+   int id;
+   String name;
+	
+	
+	void display()
+
+{
+	System.out.println("Student information : id & Name  " +id + "  "+name);
+}
+}
+
+public class DemoOnClass {
+
+	public static void main(String[] args) {
+		Student obj1 = new Student();
+//		obj1.id=12;
+//		obj1.name="anil";
+		obj1.display();
+	}
+
+}
